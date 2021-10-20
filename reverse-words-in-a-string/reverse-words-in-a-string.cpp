478cbf08451e97343e7c128e34d62152211b588a
garry133 <73153153+garry133@users.noreply.github.com>
2021-10-20 20:23:12 +0530

    Time: 8 ms (42.75%), Space: 7.3 MB (73.62%) - LeetHub

--- reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,25 +1,53 @@
+// class Solution {
+// public:
+//     string reverseWords(string s) {
+//         vector<string>v;
+//         string a;
+//         for(int i=0;i<s.size();i++){
+//             if(s[i]!=' '){
+//                 a+=s[i];
+//             }
+//             else{
+//                 if(a.size()>0)
+//                     v.push_back(a);
+//                 a="";
+//             }
+//         }
+//         if(a.size()>0)
+//         v.push_back(a);
+//         a="";
+//         for(int i=v.size()-1;i>=0;i--){
+//             a+=v[i];
+//             if(i!=0)
+//             a+=" ";
+//         }
+//         return a;
+//         // return a.substr(0,a.size()-1);
+//     }
+// };
+
 class Solution {
 public:
     string reverseWords(string s) {
-        vector<string>v;
-        string a;
-        for(int i=0;i<s.size();i++){
+        string a,ans;
+        for(int i=s.size()-1;i>=0;i--){
             if(s[i]!=' '){
                 a+=s[i];
             }
             else{
-                if(a.size()>0)
-                    v.push_back(a);
+                if(a.size()>0){
+                    reverse(a.begin(),a.end());
+                    ans+=a;
+                    ans+=" ";
+                }
                 a="";
             }
         }
-        if(a.size()>0)
-        v.push_back(a);
-        a="";
-        for(int i=v.size()-1;i>=0;i--){
-            a+=v[i];
-            a+=" ";
+        if(a.size()>0){
+            reverse(a.begin(),a.end());
+                    ans+=a;
+            return ans;
         }
-        return a.substr(0,a.size()-1);
+        else return ans.substr(0,ans.size()-1);
     }
 };
